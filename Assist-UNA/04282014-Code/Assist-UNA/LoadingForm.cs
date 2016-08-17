cb1238a3e1543cd78a438ae54ad7361fac614138
J. Michael Beaver <jmbeaver1@gmail.com>
2016-08-17 10:44:38 -0400

    Unzip of 04282014.zip

---
+++ Assist-UNA/04282014-Code/Assist-UNA/LoadingForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+
+/*************************************************************************************************
+ * 
+ * Name: InputForm class
+ * 
+ * ================================================================================================
+ * 
+ * Description: This class is a loading form used when starting ASSIST-UNA.
+ *                            
+ * ================================================================================================
+ * 
+ * Modification History
+ * --------------------
+ * 03/22/2014   ACA     Original version.
+ *   
+ *************************************************************************************************/
+namespace Assist_UNA
+{
+    public partial class LoadingForm : Form
+    {
+        public LoadingForm()
+        {
+            InitializeComponent();
+        }
+
+        private void LoadingFormShown(object sender, EventArgs e)
+        {
+            //int counter = 0;
+            //while (counter < 100)
+            //{
+            //    Thread.Sleep(500);
+            //    if (counter % 3 != 0)
+            //        txtLoading.Text += ".";
+            //    else
+            //        txtLoading.Text = "Loading ASSIST-UNA, Please wait";
+            //    this.Refresh();
+            //    Application.DoEvents();
+            //}
+        }
+    }
+}
